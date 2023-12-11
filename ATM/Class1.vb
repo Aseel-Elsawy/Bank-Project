b5b59f01952ac0fb04679c6591a27a2d0ffb88de
Aseel-Elsawy <120840192+Aseel-Elsawy@users.noreply.github.com>
2023-12-11 22:31:19 +0200

    Delete directory

--- ATM/Class1.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Security.Cryptography
-Imports Microsoft.VisualBasic
-
-
-Public Class Class1
-    Private Function GetData(sql As String) As sqldatareader
-
-    End Function
-End Class
